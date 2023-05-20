7b6230135d1008a3f7da3043d471c93806e4ed7a
Guilherme <guilhermemb11@gmail.com>
2023-05-19 22:44:22 -0300

    começo parte direita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 export default function Home() {
   return (
-    <div className="text-2xl font-bold uppercase">
-      <div className="h-screen bg-zinc-950 p-9 text-zinc-100">
-        tailwind here
-        <div className="text-4xl font-bold">Cápsula do tempo</div>
-        <h1 className="font-sans text-2xl font-bold">first h1</h1>
-        <h1 className="font-alt text-2xl font-bold">first h1</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="leadint-relaxed w-[360px] text-center">
+            Você ainda não criou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
       </div>
     </div>
   )
