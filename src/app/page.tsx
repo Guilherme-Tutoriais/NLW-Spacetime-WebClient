a57ebf367900a9faec1e93c7c69e43f72cc2154c
Guilherme <guilhermemb11@gmail.com>
2023-05-19 22:14:04 -0300

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { Button } from '@/components/Button'
-
 export default function Home() {
   return (
     <div className="text-2xl font-bold uppercase">
-      Hello
-      <Button title="oi" />
-      <Button title="dois" />
-      <Button title="tres" />
       <div className="h-screen bg-zinc-950 p-9 text-zinc-100">
-        tailwind here<div className="text-4xl font-bold">Cáśula do tempo</div>
+        tailwind here
+        <div className="text-4xl font-bold">Cápsula do tempo</div>
+        <h1 className="font-sans text-2xl font-bold">first h1</h1>
+        <h1 className="font-alt text-2xl font-bold">first h1</h1>
       </div>
     </div>
   )
